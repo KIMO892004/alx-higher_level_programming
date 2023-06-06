62aa72b9aebce4f6a7c75a66cc24036020788ce3
KIMO892004 <kimoragab2004@gmail.com>
2023-06-06 16:08:33 -0700

    kimo

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
+for k in range(ord('a'), ord('z') + 1):
+    if chr(k) == 'e' or chr(k) == 'q':
         continue
     else:
-        print("{:s}".format(chr(i)), end="")
+        print("{:s}".format(chr(k)), end='')
