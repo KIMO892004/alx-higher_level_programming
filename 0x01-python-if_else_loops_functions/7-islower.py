76d11da399e70131713a8680b088318e80527b0f
KIMO892004 <kimoragab2004@gmail.com>
2023-06-06 16:40:41 -0700

    kimo

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Author - Tolulope Fakunle
+
+def islower(k):
+    """Function checking for lowercase characters."""
+    if ord(k) >= 97 and ord(k) <= 122:
+        return True
+    else:
+        return False
