79b5a8b406dadeba3cb944f524219cdebebd2b72
KIMO892004 <kimoragab2004@gmail.com>
2023-06-06 15:58:37 -0700

    kimo

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for letters in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(letters)), end="")
+
+for i in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(i)), end="")
