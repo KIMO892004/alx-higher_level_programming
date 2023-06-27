d205ca121421252642f545cacadb06e5c9f2e0d7
KIMO892004 <kimoragab2004@gmail.com>
2023-06-27 09:43:24 -0700

    kamaal

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" defines a square """
+
+
+class Square:
+    """ square with private instance attribute size """
+
+
+    def __init__(myself, mysize=0):
+        """
+        Args:
+            size: size of square
+        """
+
+
+        if type(mysize) is int:
+            if mysize < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                myself.__size = mysize
+        else:
+            raise TypeError('size must be an integer')
