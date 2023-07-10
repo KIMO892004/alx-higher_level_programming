01ffa9a2eb332f8014b3959a441958185e19a15a
KIMO892004 <kimoragab2004@gmail.com>
2023-07-10 14:25:45 -0700

    tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Containing the lookup function
+"""
+
+
+def lookup(obj):
+    """returning the list of available attributes and methods of an object"""
+    return dir(obj)
