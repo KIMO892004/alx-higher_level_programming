a5f15fc5ca701dbefd625eef260ceb5820899330
KIMO892004 <kimoragab2004@gmail.com>
2023-07-12 14:34:39 -0700

    gr

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,34 +7,34 @@ import sys
 file_size = 0
 status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
-k = 0
+i = 0
 try:
     for line in sys.stdin:
         tokens = line.split()
         if len(tokens) >= 2:
-            a = k
+            a = i
             if tokens[-2] in status_tally:
                 status_tally[tokens[-2]] += 1
-                k += 1
+                i += 1
             try:
                 file_size += int(tokens[-1])
-                if a == k:
-                    k += 1
+                if a == i:
+                    i += 1
             except FileNotFoundError:
-                if a == k:
+                if a == i:
                     continue
-        if k % 10 == 0:
+        if i % 10 == 0:
             print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print("{:s}: {:d}".format(key, value))
+            for key, myvalue in sorted(status_tally.items()):
+                if myvalue:
+                    print("{:s}: {:d}".format(key, myvalue))
     print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+    for key, myvalue in sorted(status_tally.items()):
+        if myvalue:
+            print("{:s}: {:d}".format(key, myvalue))
 
 except KeyboardInterrupt:
     print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+    for key, myvalue in sorted(status_tally.items()):
+        if myvalue:
+            print("{:s}: {:d}".format(key, myvalue))
