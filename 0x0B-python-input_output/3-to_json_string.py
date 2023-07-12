3e5a011670c75a414be50b0cf323aebf005245f1
KIMO892004 <kimoragab2004@gmail.com>
2023-07-12 14:18:21 -0700

    k

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+containing the JSON string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returning the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
