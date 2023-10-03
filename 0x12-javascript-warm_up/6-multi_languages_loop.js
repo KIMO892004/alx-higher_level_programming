62fbcda33dabf6ea8c2f954905224fc2a3d4cc2d
KIMO892004 <kimoragab2004@gmail.com>
2023-10-03 14:31:01 -0700

    k

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-
-const sence = [
-  'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
-];
-
-for (let s = 0; s < sence.length; s++) {
-  console.log(sence[s]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
